9baeac0f7c066017ded8efa54b600969c37ba2a7
Innocent Angwadia <demsdema217@gmail.com>
2019-02-06 20:22:47 +0300

    Add Program 06 : Print alphabets

---
+++ src/Alphabets.java
@@ -0,0 +1,27 @@
+public class Alphabets {
+  public static void main(String[] args) {
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+      System.out.println(ch);
+  }
+}
+
+// USING WHILE AND DO LOOP
+/**
+ * char c = 'a';
+ * <p>
+ * while (c <= 'z') {
+ * System.out.println(c);
+ * c++;
+ * }
+ * Using a do while loop:
+ * <p>
+ * char c = 'A';
+ * <p>
+ * do {
+ * System.out.println(c);
+ * c++;
+ * } while (c <= 'Z');
+ * }
+ */
